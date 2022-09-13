52c89a085f13e580bf20aa239f3fcf7578eb63db
Andreas Müller <andreas@mueller.bet>
2022-09-13 09:03:15 +0200

    Design angepasst

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,4 +1,5 @@
 import 'package:diabetes_weight_app/src/app.dart';
+import 'package:diabetes_weight_app/src/style_constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -13,22 +14,146 @@ class HomeScreen extends ConsumerWidget {
           "Diabetes Kohlenhydrate Rechner",
         ),
       ),
-      body: Column(
-        children: [
-          DropdownButton(
-            value: ref.watch(themeModeProvider.state).state ?? ThemeMode.system,
-            items: ThemeMode.values
-                .map(
-                  (e) => DropdownMenuItem(
-                    value: e,
-                    child: Text(e.name),
+      body: Padding(
+        padding: const EdgeInsets.all(defaultSpace),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Flexible(
+                  child: Placeholder(
+                    child: TextFormField(),
                   ),
-                )
-                .toList(),
-            onChanged: (value) =>
-                ref.read(themeModeProvider.state).state = value,
-          ),
-        ],
+                ),
+                const SizedBox(
+                  width: defaultSpace,
+                ),
+                const IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.search),
+                ),
+              ],
+            ),
+            Row(
+              children: [
+                const IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.remove),
+                ),
+                const SizedBox(
+                  width: defaultSpace,
+                ),
+                Flexible(
+                  child: TextFormField(
+                    initialValue: "0.00",
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(width: defaultSpace),
+                const IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.add),
+                ),
+                const SizedBox(width: defaultSpace),
+                const IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.scale),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: defaultSpace,
+            ),
+            const Divider(
+              thickness: 1,
+            ),
+            const SizedBox(
+              height: defaultSpace,
+            ),
+            Flexible(
+              child: DataTable(
+                headingRowColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                  return Theme.of(context).colorScheme.primary;
+                }),
+                columns: const [
+                  DataColumn(
+                    label: Text("Produkt"),
+                  ),
+                  DataColumn(
+                    label: Text("Gewicht"),
+                  ),
+                  DataColumn(
+                    label: Text("KH"),
+                  ),
+                  DataColumn(
+                    label: Text("Zucker"),
+                  ),
+                ],
+                rows: [
+                  const DataRow(
+                    cells: [
+                      DataCell(
+                        Text(
+                          "Auswahl treffen",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      DataCell(
+                        Text(""),
+                      ),
+                      DataCell(
+                        Text(""),
+                      ),
+                      DataCell(
+                        Text(""),
+                      ),
+                    ],
+                  ),
+                  const DataRow(
+                    cells: [
+                      DataCell(
+                        Text(
+                          "Gesamt",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      DataCell(
+                        Text(
+                          "0 g",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      DataCell(
+                        Text(
+                          "0 g",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      DataCell(
+                        Text(
+                          "0 g",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/src/style_constants.dart
@@ -0,0 +1,3 @@
+const double defaultSpace = 16;
+const double smallSpace = 8;
+const double largeSpace = 32;
